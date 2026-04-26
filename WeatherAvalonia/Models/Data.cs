09d9233b356a732008990af00aff678351625e4d
CYBER ARTEL <CYBER ARTEL@DESKTOP-AQOEL74>
2026-04-26 08:45:56 +0300

    сделал сервис и добавил модель

---
+++ WeatherAvalonia/Models/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherAvalonia.Models
+{
+    internal class Data
+    {
+        [Newtonsoft.Json.JsonProperty("temp")]
+        public double Temp { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("pressure")]
+        public double Pressure { get; set; }
+    }
+}
---
+++ WeatherAvalonia/Servises/WeatherService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherAvalonia.Servises
+{
+    internal class WeatherService
+    {
+        private readonly HttpClient _httpClient = new();
+
+        private const string ApiKey = "601f11387b007836cf77827c9bf290b9";
+        private const string BaseUrl = "https://api.openweathermap.org/data/3.0/weather";
+
+        public async Task<Models.Data> GetWeatherAsync(string city)
+        {
+            string url = $"{BaseUrl}?q={city}&units=metric&appid={ApiKey}&lang=ru";
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Models.Data>(json);
+        }
+    }
+}
